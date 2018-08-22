e613033ecc7b40fcce3375cd76edc75e8a2724d5
martem4 <martem4@yandex.ru>
2018-08-22 18:03:05 +0300

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
 import listener.SysEventListener;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 
 public class Main {
     public static void main(String[] args) {
